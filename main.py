a54ca4e8c9b89d2a15bc80a477eb9a0ece51a981
Mateusz Rusin <matrus@boozt.com>
2024-05-20 00:30:54 +0200

    Add Pandas

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 # - *- coding: utf- 8 - *-
 from parole import Parole
+import time
 
 length = int(input('Jak długiego zlepku spółgłosek szukasz? '))
 parole = Parole(length)
+
+start_time = time.time()
+words = parole.get_by_pandas()
+end_time = time.time()
+
+print("Znaleziono {} zlepków".format(sum(len(sublist) for sublist in words.values())))
+print(f"Czas wykonania funkcji get_by_pandas(): {end_time - start_time:.4f} sekund")
+
+parole.show(words)
+
+start_time = time.time()
 words = parole.get()
-cnt = len(words)
-print("Znaleziono {} zlepków".format(cnt))
+end_time = time.time()
+
+print("Znaleziono {} zlepków".format(sum(len(sublist) for sublist in words.values())))
+print(f"Czas wykonania funkcji get(): {end_time - start_time:.4f} sekund")
+
 parole.show(words)
--- parole.py
+++ parole.py
@@ -2,26 +2,25 @@
 import io
 import json
 import re
+import pandas as pd
 
 
 class Parole:
-    def __init__(self, length):
+    def __init__(self, length, filepath='slowa.txt'):
         self.length = length
         self.consonants = re.compile(r"[bcćdfghjklłmnńprstwxzźż]", re.UNICODE)
         self.doubles = re.compile(r"rz|cz|sz|dz|dź|dż|ch", re.UNICODE)
-        self.filepath = 'slowa.txt'
+        self.filepath = filepath
 
     def show(self, items):
         print(self.json(items))
 
-    def count_consonants(self, word):
-        # Przechodzi przez słowo, zliczając spółgłoski i uwzględniając podwójne spółgłoski
-        count = 0
-        i = 0
+    def count(self, word):
+        count = i = 0
         while i < len(word):
             if self.doubles.match(word, i):
                 count += 1
-                i += 2  # Przeskakujemy podwójną spółgłoskę
+                i += 2
             elif self.consonants.match(word[i]):
                 count += 1
                 i += 1
@@ -38,8 +37,21 @@ class Parole:
         cores = {}
         with io.open(self.filepath, mode="r", encoding="utf-8") as fp:
             for line in fp:
-                if self.count_consonants(line):
+                if self.count(line):
                     cores.setdefault(len(line), []).append(line.strip())
+
+        return cores
+
+    def get_by_pandas(self):
+        df = pd.read_csv(self.filepath, header=None, names=['word'], encoding='utf-8')
+        df['word'] = df['word'].str.strip()
+
+        # Filtracja słów spełniających warunki
+        df['valid'] = df['word'].apply(self.count)
+        valid_words = df[df['valid']]['word']
+
+        # Grupowanie według długości słowa
+        cores = valid_words.groupby(valid_words.str.len()).apply(list).to_dict()
 
         return cores
 
